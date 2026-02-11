2ad0c097c7c4a9a711b5f985400bd8e7ee29e02d
Kolyah35 <kolyah35@gmail.com>
2026-02-11 20:34:06 +0300

    wow calcos from zero / I guess now we make esp32s3 os

--- src/drv/analog.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <stdint.h>
-
-#define DEFAULT 0
-
-void analog_write(uint8_t pin, uint16_t duty);
-uint16_t analog_read(uint8_t pin);
--- src/firmwares.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <platform.h>
-#include "blink500.h"
-#include "blink1000.h"
-
-// char* const g_firmwares_bytes[] = {blink500, blink1000};
-// PGM_P g_firmwares_names[] = {"blink500.bin", "blink1000.bin"};
